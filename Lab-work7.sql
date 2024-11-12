9fc583e9446a0117450b411dd27fdf302654b958
araika06 <156989819+araika06@users.noreply.github.com>
2024-11-12 15:19:35 +0500

    Add files via upload

--- Lab-work7.sql
+++ Lab-work7.sql
@@ -39,22 +39,38 @@ insert into employees(first_name, last_name, salary, department_id) values('John
                                                                           ('Ava', 'Johnson', 48000, 5);
 
 --1
-create index idx_countries_name on countries(name);
+create index idx_countries_name on countries(name)
+where name =  'USA';
+
+SELECT * FROM countries WHERE name = 'USA';
+
+drop index idx_countries_name;
 
 --2
-create index idx_employees_name_surname on employees(first_name, last_name);
+create index idx_employees_name_surname on employees(first_name, last_name)
+where first_name = 'John' and last_name = 'Doe';
+
+drop index idx_employees_name_surname;
 
 --3
-create unique index idx_salary on employees(salary);
+create unique index idx_salary on employees(salary) where salary < 70000 and salary > 55000;
+
+drop index idx_salary;
 
 --4
-create index idx_sub_name on employees(substring(first_name from 1 for 4));
+create index idx_sub_name on employees(substring(first_name from 1 for 4))
+where (substring(first_name from 1 for 4)) = 'live';
 
+drop index idx_sub_name;
 
 --5
-create index idx_depatrments_budget_salary on employees(department_id, salary);
-create index idx_departments_budget on departments(budget);
+create index idx_depatrments_budget_salary on employees(department_id, salary)
+where salary < 65000;
+create index idx_departments_budget on departments(budget)
+where budget > 200000;
 
+drop index idx_depatrments_budget_salary;
+drop index idx_departments_budget;
 
 drop database lab7;
 drop table countries;
